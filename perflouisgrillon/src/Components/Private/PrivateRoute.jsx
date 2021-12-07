55519160f49f883ec7978565b559c3212697556a
PazzeG <guillaume.pazze@gmail.com>
2021-12-07 16:42:27 +0100

    fix_useStorage_07/12/21

--- perflouisgrillon/src/Components/Private/PrivateRoute.jsx
+++ perflouisgrillon/src/Components/Private/PrivateRoute.jsx
@@ -1,9 +1,9 @@
 import React from 'react'
 import {Navigate} from 'react-router-dom'
-import { auth} from '../../Firebase'
+import { auth } from '../../Firebase'
 
-function PrivateRoute({component: Component}) {
+function PrivateRoute({children}) {
     const user = auth.currentUser
-    return user ? <Component /> : <Navigate to='/login'/> 
+    return user ? children : <Navigate to='/login' replace /> 
     }
 export default PrivateRoute
--- perflouisgrillon/src/hooks/useStorage.jsx
+++ perflouisgrillon/src/hooks/useStorage.jsx
@@ -1,5 +1,6 @@
-import {projectStorage, projectFirestore, timestamp} from '../Firebase'
+import {projectStorage, timestamp} from '../Firebase'
 import { useEffect, useState } from 'react'
+import { ref as storageRef, uploadBytesResumable, getDownloadURL } from "firebase/storage";
 
 
 function useStorage(file) {
@@ -8,25 +9,42 @@ function useStorage(file) {
     const [url, setUrl] = useState(null);
    
     useEffect(() => {
-
-        const storageRef = projectStorage.ref(file.name)
-        const collectionRef = projectFirestore.collection('images');
-
-        storageRef.put(file).on('state_changed', (snap) => {
-            let percent = (snap.bytesTransferred / snap.totalBytes) * 100;
-            setProgress(percent >> 0); // or Math.trunc() 
-        }, (err) => {
-            setError(err);
-        }, async () =>{
-            const url = await storageRef.getDownloadURL();
-            const createdAt = timestamp();
-            collectionRef.add({ url, createdAt});
-            setUrl(url);
+        const storageReference = storageRef(projectStorage, file.name);
+        const uploadTask = uploadBytesResumable(storageReference, file);
+      
+        uploadTask.on("state_changed", (snapshot) => {
+            console.log(snapshot + "hello je suis la snapshot");
+            let percent = (snapshot.bytesTransferred / snapshot.totalBytes) * 100;
+                setProgress(percent >> 0); 
+            }, (err) => {
+                setError(err);
+            }, async () =>{
+                const url = await getDownloadURL(storageRef);
+                setUrl(url);
+                console.log('hey ' + url );
         });
-
-    }, [file]);
+      }, [file])
 
     return {progress, url, error};
 }
 
 export default useStorage;
+
+// useEffect(() => {
+
+//     const storageRef = projectStorage.ref(file.name)
+//     const collectionRef = projectFirestore.collection('images');
+
+//     storageRef.put(file).on('state_changed', (snap) => {
+//         let percent = (snap.bytesTransferred / snap.totalBytes) * 100;
+//         setProgress(percent >> 0); // or Math.trunc() 
+//     }, (err) => {
+//         setError(err);
+//     }, async () =>{
+//         const url = await storageRef.getDownloadURL();
+//         const createdAt = timestamp();
+//         collectionRef.add({ url, createdAt});
+//         setUrl(url);
+//     });
+
+// }, [file]);
