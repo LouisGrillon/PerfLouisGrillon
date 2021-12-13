18453ea750e9dc10532ff67cf89f58c7d6de5046
PazzeG <guillaume.pazze@gmail.com>
2021-12-13 09:09:18 +0100

    test_build_13/12/2021

--- perflouisgrillon/src/Firebase.js
+++ perflouisgrillon/src/Firebase.js
@@ -5,12 +5,12 @@ import {getAuth} from'firebase/auth'
 
 
 const firebaseConfig = {
-    apiKey: import.meta.env.production.VITE_APP_API_KEY,
-    authDomain: import.meta.env.production.VITE_APP_AUTH_DOMAIN,
-    projectId: import.meta.env.production.VITE_APP_PROJECT_ID,
-    storageBucket: import.meta.env.production.VITE_APP_STORAGE_BUCKET,
-    messagingSenderId: import.meta.env.production.VITE_APP_MESSAGING_SENDER_ID,
-    appId: import.meta.env.productionuction.VITE_APP_APP_ID
+    apiKey: import.meta.env.VITE_APP_API_KEY,
+    authDomain: import.meta.env.VITE_APP_AUTH_DOMAIN,
+    projectId: import.meta.env.VITE_APP_PROJECT_ID,
+    storageBucket: import.meta.env.VITE_APP_STORAGE_BUCKET,
+    messagingSenderId: import.meta.env.VITE_APP_MESSAGING_SENDER_ID,
+    appId: import.meta.env.VITE_APP_APP_ID
     // measurementId: import.meta.env.VITE_APP_MEASUREMENT_ID
 }
 
