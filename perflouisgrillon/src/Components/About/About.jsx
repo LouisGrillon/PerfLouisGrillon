cd4d0444899138b2d45ec6fec7feae77c7f5fe16
PazzeG <guillaume.pazze@gmail.com>
2021-12-19 23:49:19 +0100

    new_trad_19/12/2021

--- perflouisgrillon/src/Components/About/About.jsx
+++ perflouisgrillon/src/Components/About/About.jsx
@@ -22,21 +22,25 @@ function About() {
                 <span className='description'>2004 - {t('description_part_2')} <span className="important_Item"> Trialistes de L'Ardoux</span>
                 {t('description_part_3')} Mézières-lez-Cléry (45).</span> <br />
                 <span className='description'>2005 - {t('description_part_4')}</span> <br />
-                <span className='description'>2004 - 2016 - {t('description_part_5')} <span className="important_Item"> Les Trialistes de l'Ardoux</span></span> <br />
-                <span className='description'>2017 - {t('description_part_6')}<span className="important_Item">Horizon Monluçon</span> {t('description_part_7')}</span>
+                <span className='description'>2004 - 2016 - {t('description_part_5')} <span className="important_Item"> Les Trialistes de l'Ardoux</span> {t('and')} Trial Club Cerny</span> <br />
+                <span className='description'>2017 - 2021 {t('description_part_6')}<span className="important_Item">Horizon Monluçon</span></span><br />
+                <span className='description'>2022 - {t('description_part_6')}{t('description_part_3_5')} <span className="important_Item">Trial Club Saint Gervais</span></span>
             </div>
 
             <div className="trophies_wrapper" >
                 <h2 className="trophies_Title">{t('trophies_Title')}</h2>
-                <span className='description'>2009 - {t('description_part_8')} <span className="important_Item">{t('description_part_9')}</span></span> <br />
-                <span className='description'>2015 - {t('description_part_10')} <span className="important_Item">{t('description_part_11')}</span></span> <br />
-                <span className='description'>2015 - {t('description_part_12')} <span className="important_Item">{t('description_part_13')}</span>{t('description_part_14')}</span> <br />
-                <span className='description'>2015 - {t('description_part_15')} </span><br />
+                <span className='description'>2009 - {t('description_part_8')} <span className="important_Item">{t('description_part_9')}</span>{t('description_part_9_add_1')}</span> <br />
+                <span className='description'>2010 - {t('description_part_925')} <span className="important_Item">{t('description_part_9')}</span>{t('description_part_11')}</span> <br />
+                <span className='description'>2011 - {t('description_part_9_add_2')} <span className="important_Item">{t('description_part_9')}</span>{t('description_part_11')}</span> <br />
+                <span className='description'>2015 - {t('description_part_10')} <span className="important_Item">{t('description_part_9')}</span></span> <br />
+                <span className='description'> - {t('description_part_12')} <span className="important_Item">{t('description_part_13')}</span>{t('description_part_14')}</span> <br />
+                <span className='description'> - {t('description_part_15')} </span><br />
                 <span className='description'>2016 - 2017 - {t('description_part_16')} <span className="important_Item">{t('description_part_17')}</span></span> <br />
                 <span className='description'>2017 - {t('description_part_18')} <span className="important_Item">{t('description_part_19')}</span></span> <br />
+                <span className='description'> - {t('description_part_22')} <span className="important_Item">{t('description_part_23')}</span></span> <br />
                 <span className='description'>2017 - 2019 - 2020 - {t('description_part_20')} <span className="important_Item">{t('description_part_21')}</span></span> <br />
-                <span className='description'>2017 - {t('description_part_22')} <span className="important_Item">{t('description_part_23')}</span></span> <br />
-                <span className='description'>2021 - {t('description_part_24')} <span className="important_Item">{t('description_part_25')}</span></span>
+                <span className='description'>2021 - {t('description_part_24')} <span className="important_Item">{t('description_part_25')}</span></span><br />
+                <span className='description'>- n°7 <span className="important_Item">{t('description_part_25')}</span></span>
             </div>
 
             <div className="degrees_wrapper" >
