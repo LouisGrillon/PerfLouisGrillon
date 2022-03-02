d75f7a094dd68a9d1cf9d9494641d94e3748a958
PazzeG <guillaume.pazze@gmail.com>
2022-03-02 13:17:53 +0100

    new-partners

--- perflouisgrillon/src/Components/Partners/Partners.jsx
+++ perflouisgrillon/src/Components/Partners/Partners.jsx
@@ -3,6 +3,7 @@ import Layout from '../Layout/Layout'
 import lasalle from '../../Assets/lasalle.jpg'
 import ophycure from '../../Assets/ophycure.jpg'
 import volodalen from'../../Assets/volodalen.jpg'
+import  trialbikes from '../../Assets/trialbikes.jpg'
 import './Partners.css'
 
 function Partners() {
@@ -10,7 +11,7 @@ function Partners() {
         <div>
             <div className="slider" id="slider">
                 <figure>
-                    <a href="https://www.trial-bikes.com/"><img  src={trialbikes} alt="trialbikes"  id="trialbikes"/></a>  
+                    <a href="https://www.trial-bikes.com/"><img  src={trialbikes} alt="trialbikes"  id="trialbikes"/></a>
                     <a href="https://ophycure.com"><img src={ophycure} alt="ophycure" id="ophycure" /></a>
                     <a href="https://volodalen.com/"><img src={volodalen} alt="volodalen" id="volodalen" /></a>
                     <a href="https://lasalle-coaching.fr/"><img src={lasalle} alt="lasalle" id="lasalle"/></a>
