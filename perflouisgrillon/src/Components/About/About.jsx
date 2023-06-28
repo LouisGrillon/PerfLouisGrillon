aed56d8b9888ddaa36a7da756c616ad98403b9fe
Guillaume Pazzé <66828481+PazzeG@users.noreply.github.com>
2023-06-28 14:25:36 +0200

    Update About.jsx

--- perflouisgrillon/src/Components/About/About.jsx
+++ perflouisgrillon/src/Components/About/About.jsx
@@ -43,7 +43,7 @@ function About() {
                 <span className='description'>2021 - <span>{t('description_part_9_add_2')}<span className="important_Item">{t('description_part_16')}</span>{t('description_part_17')}</span></span><br />
                 <span className='description'>2021 - n°7 <span className="important_Item">{t('description_part_25')}</span></span><br />
                 <span className='description'>2022 - n°9 <span className="important_Item">{t('description_part_25')}</span></span><br />
-                <span className='description'>2022 - <span className="important_Item">{t('description_part_8_5')}</span>{t('description_part_17')}</span><br />
+                <span className='description'>2022 - <span className="important_Item">{t('description_part_18_5')}</span>{t('description_part_17')}</span><br />
             </div>
 
             <div className="degrees_wrapper" >
